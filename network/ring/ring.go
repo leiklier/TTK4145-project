0deac82a74b6de2cecf343bacb0a4e17d0fd5ed8
Stens <kris.ste@online.no>
2020-02-22 14:54:15 +0100

    Prototype functions

--- network/ring/ring.go
+++ network/ring/ring.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"syscall"
+	"time"
 
 	"../peers"
 )
@@ -13,58 +13,60 @@ import (
 const gPORT = 1567
 const gBroadcastIP = "255.255.255.255"
 
-var HEAD = true // Has to be changed
+var gHEAD = false // Has to be changed
 const gConnectAttempts = 5
 const gBCASTPING = "RING EXISTS"
+const gTIMEOUT = 2
 
+// Initializes the network if it's present. Establishes a new network if not
 func Init() {
 	var ringExists = false
-	ln, err := net.ListenPacket("udp", "255.255.255.255:"+PORT) // Maybe have helper function returning ln
-	buffer := make([]byte, 1024)                                // What happens if packet > buffer
+	buffer := make([]byte, 1024) // What happens if packet > buffer
+	conn := dialBroadcastUDP(gPORT)
+	addr, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("%s:%d", gBroadcastIP, gPORT))
+	conn.SetReadDeadline(time.Now().Add(gTIMEOUT)) // Timeout after 2 sec
 
-	if err != nil {
-		fmt.Println("Unable to listen to udp broadcast")
-		fmt.Println(err)
-	}
-
-	defer ln.Close()
+	defer conn.Close()
 
 	for i := 0; i < gConnectAttempts; i++ {
-		nBytes, addr, err := ln.ReadFrom(buffer)
+		nBytes, addr, err := conn.ReadFrom(buffer)
 		if nBytes > 0 {
-			ringExists = true
+			if string(buffer[:nBytes]) == gBCASTPING {
+				ringExists = true
+				break
+			}
 		}
-		
 	}
-
+	if ringExists {
+		sendJoinMSG()
+		// Start tcp ring server?
+	} else {
+		gHEAD = true
+		go Listenjoin()
 	}
+	// go peers.Server()
 
 }
-
 
 // Only runs if you are HEAD, listen for new machines broadcasting
 // on the network using UDP. The new machine is added to the list of
 // known machines. That list is propagted trpough the ring to update the ring
 func Listenjoin() {
-	if !HEAD { // Hmmm, has to be changed
+	if !gHEAD { // Hmmm, has to be changed
 		return
 	}
 	buffer := make([]byte, 1024) // What happens if packet > buffer
 
-	ln, err := net.ListenPacket("udp", "255.255.255.255:"+strconv.Itoa(gPORT))
+	conn := dialBroadcastUDP(gPORT)
+	conn.SetReadDeadline(time.Now().Add(gTIMEOUT)) // Timeout after 2 sec
 
-	if err != nil {
-		fmt.Println("Unable to listen to udp broadcast")
-		fmt.Println(err)
-	}
-
-	defer ln.Close()
+	defer conn.Close()
 
 	for {
-		nBytes, addr, err := ln.ReadFrom(buffer)
+		nBytes, addr, err := conn.ReadFrom(buffer)
 		if nBytes > 0 {
 			peers.AddTail(string(buffer[:nBytes]))
-			err = ring.Broadcast(peers.GetAll())
+			err = ring.addNode(peers.GetAll())
 			if err != nil {
 				fmt.Println("Failed to broadcast")
 				fmt.Println(err)
@@ -72,6 +74,14 @@ func Listenjoin() {
 		}
 
 	}
+}
+
+func addNode(peersList []string) {
+	// peers.Send(msg)
+}
+
+func listenBroadcast() {
+
 }
 
 // send msg to 255
