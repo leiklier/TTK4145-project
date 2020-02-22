23e0983a2aa124220cf7ca780bc4160e6f7eaede
Stens <edmoe@stud.ntnu.no>
2020-02-22 19:06:19 +0100

    Updated to support messages

--- network/ring/ring.go
+++ network/ring/ring.go
@@ -1,12 +1,15 @@
 package ring
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
 	"net"
 	"os"
 	"syscall"
 	"time"
 
+	"../../store"
 	"../messages"
 	"../peers"
 )
@@ -28,18 +31,16 @@ import (
 const ( // Ugh
 	NodeChange = "NodeChange"
 	CallList   = "CallList"
+	Maintain   = "Maintain"
 )
 
-const gPORT = 1567
+const gPORT = 6971
 const gBroadcastIP = "255.255.255.255"
 
 var gHEAD = false // Has to be changed
 const gConnectAttempts = 5
 const gBCASTPING = "RING EXISTS"
 const gTIMEOUT = 2
-
-
-order_chn := make(chan store.ElevatorOrder)
 
 // Initializes the network if it's present. Establishes a new network if not
 func Init() {
@@ -69,7 +70,8 @@ func Init() {
 	}
 
 	go maintainRing()
-	go listenCalls()
+	go listenCallsIn()
+	go listenCallsOut()
 }
 
 // Only runs if you are HEAD, listen for new machines broadcasting
@@ -90,8 +92,10 @@ func Listenjoin() {
 		nBytes, addr, err := conn.ReadFrom(buffer)
 		if nBytes > 0 {
 			peers.AddTail(string(buffer[:nBytes]))
-			msg := messages.Message{messages.Broadcast, AddNode, []byte(peersList)}
-			messages.Send(msg)
+			buf := &bytes.Buffer{}
+			gob.NewEncoder(buf).Encode(peers.GetAll())
+			bs := buf.Bytes()
+			messages.SendMessage(NodeChange, bs)
 			if err != nil {
 				fmt.Println("Failed to broadcast")
 				fmt.Println(err)
@@ -102,23 +106,22 @@ func Listenjoin() {
 
 func maintainRing() {
 	for {
-		msg := messages.Recieve(NodeChange)
-		peers.Set(string(msg.Data))
-		messages.Send(msg)
+		nodes := messages.Receive(NodeChange)
+		fmt.Println(string(nodes))
+		//peers.Set(string(nodes))
+		messages.SendMessage(Maintain, nodes)
 	}
 }
-
 
 func listenCallsIn() {
 	for {
 		msg := messages.Receive(CallList)
-		store.reciveElevState <- msg.Data
+		store.reciveElevState <- msg
 	}
 }
 
-
 func listenCallsOut() {
-	for{
+	for {
 		states := <-store.SendElevState
 		msg := messages.Message{CallList, messages.Broadcast, states}
 		messages.Send(msg)
