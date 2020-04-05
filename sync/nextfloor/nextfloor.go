c75ea85eaca805ce96d2f234ca0b482aba23dd68
Jakob Vahlin <jakobvahlin@gmail.com>
2020-04-05 23:25:09 +0200

    NextFloor function is shorter!

--- sync/nextfloor/nextfloor.go
+++ sync/nextfloor/nextfloor.go
@@ -1,9 +1,13 @@
 package nextfloor
 
 import (
+	"fmt"
 	"math/rand"
-
 	"time"
+
+	"../../network/peers"
+	"../elevators"
+	"../store"
 )
 
 // SubscribeToDestinationUpdates
@@ -310,23 +314,100 @@ func SubscribeToDestinationUpdates(nextFloor chan int) {
 	}
 }
 
-// func SubscribeToDestinationUpdates2(nextFloor <-chan int) {
-// 	for {
-// 		time.Sleep(time.Duration(2 * time.Second))
+// Kan kjøres både som goroutine eller som funksjonskall
+//
 
-// 		cabCalls, _ := store.GetAllCabCalls(selfIP)
-// 		hallCalls, _ := store.GetAllHallCalls(selfIP)
-// 		currDir, _ := store.GetDirectionMoving(selfIP)
+func SubscribeToDestinationUpdates2(nextFloor chan int) {
+	for {
+		cabCalls, _ := store.GetAllCabCalls(peers.GetRelativeTo(peers.Self, 0))
+		hallCalls, _ := store.GetAllHallCalls(peers.GetRelativeTo(peers.Self, 0))
+		currDir, _ := store.GetDirectionMoving(peers.GetRelativeTo(peers.Self, 0))
+		currFloor, _ := store.GetCurrentFloor(peers.GetRelativeTo(peers.Self, 0))
 
-// 		switch currDir {
-// 		case elevators.DirectionDown:
-// 			// Do smth
-// 		case elevators.DirectionUp:
-// 			// Do smth
-// 		case elevators.DirectionIdle:
-// 			// So smth
-// 		default:
-// 			// Da må det vel være dir both da.. det er jo litt synd om det skjer
-// 		}
-// 	}
-// }
+		var nextCab int
+
+		switch currDir {
+		case elevators.DirectionDown:
+			// Looking for hot single moms downstairs...
+			for i := store.NumFloors - 1; i >= 0; i-- {
+				if i >= currFloor {
+					continue // Sjekker kun nedover
+				} else {
+					if cabCalls[i] {
+						nextCab = i
+						nextFloor <- nextCab
+						break
+					}
+				}
+			}
+			for i := store.NumFloors - 1; i >= 0; i-- {
+				if i >= currFloor {
+					continue // Sjekker kun nedover
+				} else {
+					if hallCalls[i].Direction != elevators.DirectionIdle {
+						nextHall := i
+						if nextHall < nextCab {
+							nextFloor <- nextHall
+						}
+						break
+					}
+				}
+			}
+		case elevators.DirectionUp:
+			// Looking for hot single moms upstairs...
+			for i, cab := range cabCalls {
+				if i <= currFloor {
+					continue // Vi sjekker kun oppover
+				} else if cab {
+					nextCab = i
+					nextFloor <- nextCab
+					break
+				}
+			}
+			for i, hc := range hallCalls {
+				if i <= currFloor {
+					continue // Vi sjekker kun oppover
+				} else {
+					if hc.Direction != elevators.DirectionIdle {
+						nextHall := i
+						if nextHall < nextCab {
+							nextFloor <- nextHall
+						}
+						break
+					}
+				}
+			}
+			// Denne kan egntlig håndere hele suppen hvis vi kun sjekker på idle
+		case elevators.DirectionIdle:
+			// Looking for hot single moms everywhere
+			for counter := 1; counter < store.NumFloors; counter++ {
+				lowerSearchIndex := currFloor - counter  // Sjekk om er under 0
+				higherSearchIndex := currFloor + counter // Sjekk om er over NumFloors-1
+
+				if !(lowerSearchIndex < 0) {
+					if cabCalls[lowerSearchIndex] {
+						nextFloor <- lowerSearchIndex
+						break
+					}
+					if hallCalls[lowerSearchIndex].Direction != elevators.DirectionIdle {
+						nextFloor <- lowerSearchIndex
+						break
+					}
+				}
+				if !(higherSearchIndex > store.NumFloors-1) {
+					if cabCalls[higherSearchIndex] {
+						nextFloor <- higherSearchIndex
+						break
+					}
+					if hallCalls[higherSearchIndex].Direction != elevators.DirectionIdle {
+						nextFloor <- higherSearchIndex
+						break
+					}
+				}
+
+			}
+		default:
+			fmt.Println("Get the Bible and pray!")
+		}
+	}
+}
